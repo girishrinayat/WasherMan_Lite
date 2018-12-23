8b70547c37246f904c52dcbc502cb65ec46d052f
grinayat <grinayat@gmail.com>
2018-12-23 15:57:13 +0530

    Initial commit

---
+++ app/src/main/java/washerman/com/washerman_lite/MainActivity.java
@@ -0,0 +1,13 @@
+package washerman.com.washerman_lite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
