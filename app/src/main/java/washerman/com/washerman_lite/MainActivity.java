14d9a30d0ee766b67d153f673826c68450256d26
grinayat <grinayat@gmail.com>
2019-01-14 23:07:44 +0530

    design main menu
    Signed-off-by: grinayat <grinayat@gmail.com>

--- app/src/main/java/washerman/com/washerman_lite/MainActivity.java
+++ app/src/main/java/washerman/com/washerman_lite/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.sharedpreference.SharedPreferenceTest;
@@ -18,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferenceTest sharedPreference;
     private Boolean flag;
     private ProgressDialog progressDialog;
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         /**
          *  Assigning Activity this to progress dialog.
          */
@@ -51,13 +56,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     *assign layout to toolbar
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    /**
+     * event for toolbar
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                finish();
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                return true;
+
+            case R.id.history:
+                finish();
+                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+                return true;
+
+            case R.id.logout:
+                sharedPreference.logouts();
+                finish();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                return true;
+
+            case R.id.exit:
+                finish();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    /**
      * runtime network state
      * @return network state
      */
     private boolean isNetworkAvailable() {
         ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-
         return activeNetwork != null &&activeNetwork.isConnectedOrConnecting();
     }
 }
--- app/src/main/java/washerman/com/washerman_lite/RegisterActivity.java
+++ app/src/main/java/washerman/com/washerman_lite/RegisterActivity.java
@@ -5,14 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,7 +23,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
- * @author GIRISH
+ * @author GIRISH RINAYAT
  */
 public class RegisterActivity extends AppCompatActivity {
     private EditText eFullName;
